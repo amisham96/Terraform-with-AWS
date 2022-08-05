73549e6d7703c1c15d9b0b439e4beddc2393c218
Amit Sharma <amisharm46@publicisgroupe.net>
2022-08-05 12:11:10 +0530

    ec2 instance using terraform

---
+++ ec2-terraform/vm.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "my-first-instance" {
+  ami                         = "ami-08d4ac5b634553e16"
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  security_groups             = [var.security_group]
+  associate_public_ip_address = true
+  user_data                   = file("${path.module}/scripts/startup.sh")
+}
