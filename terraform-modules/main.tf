84e8945d055206982113a75715275d5d1f4a85ec
Amit Sharma <amisharm46@publicisgroupe.net>
2022-08-23 12:55:46 +0530

    terraform excercise

--- terraform-modules/main.tf
+++ terraform-modules/main.tf
@@ -12,5 +12,4 @@ resource "aws_instance" "my-first-instance" {
   key_name                    = module.key_pair.key-name
   security_groups             = [module.security-group.security-group-name]
   associate_public_ip_address = true
-  # user_data                   = file("${path.module}/scripts/startup.sh")
 }
